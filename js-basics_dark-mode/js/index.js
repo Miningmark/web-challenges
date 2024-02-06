29dc4d56d9b1a467525ed4fcb3c21945771e1086
Markus <markus.sibbe@t-online.de>
2024-02-06 11:33:52 +0100

    add   Challenges: JS Basics

---
+++ js-basics_dark-mode/js/index.js
@@ -0,0 +1,24 @@
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkMode = document.querySelector('[ data-js="dark-mode-button"]');
+const lightMode = document.querySelector('[data-js="light-mode-button"]');
+const toggleMode = document.querySelector('[data-js="toggle-button"]');
+
+
+darkMode.addEventListener("click", () => {
+    bodyElement.classList.remove("button--light");
+    bodyElement.classList.add("button--dark");
+});
+
+lightMode.addEventListener("click", () => {
+    bodyElement.classList.remove("button--dark");
+    bodyElement.classList.add("button--light");
+});
+
+toggleMode.addEventListener("click", () => {
+    bodyElement.classList.toggle("button--dark");
+});
+
+
