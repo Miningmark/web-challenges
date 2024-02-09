31bd2eae91ae4a097162b8b571cf9d2e8be39d80
Markus <markus.sibbe@t-online.de>
2024-02-09 12:32:03 +0100

    fix pizza input to number

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,11 +9,10 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 
-
 pizzaInput1.addEventListener("input", () => {
   // write your code here
-  let pizzaSize1 = pizzaInput1.value;
-  let pizzaSize2 = pizzaInput2.value;
+  let pizzaSize1 = Number(pizzaInput1.value);
+  let pizzaSize2 = Number(pizzaInput2.value);
   updatePizzaDisplay(pizza1, pizzaSize1);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updateOutputColor(pizzaSize1, pizzaSize2);
@@ -21,8 +20,8 @@ pizzaInput1.addEventListener("input", () => {
 
 pizzaInput2.addEventListener("input", () => {
   // write your code here
-  let pizzaSize1 = pizzaInput1.value;
-  let pizzaSize2 = pizzaInput2.value;
+  let pizzaSize1 = Number(pizzaInput1.value);
+  let pizzaSize2 = Number(pizzaInput2.value);
   updatePizzaDisplay(pizza2, pizzaSize2);
   calculatePizzaGain(pizzaSize1, pizzaSize2);
   updateOutputColor(pizzaSize1, pizzaSize2);
@@ -35,9 +34,6 @@ function calculatePizzaGain(diameter1, diameter2){
   let area2 = Math.PI * (diameter2 / 2) * (diameter2 / 2);
   let gain = Math.round((area2 - area1) / area1 * 100);
   output.value = gain;
-  console.log(area1);
-  console.log(area2);
-  console.log(gain);
 }
 
 // Task 2
@@ -49,6 +45,8 @@ function updatePizzaDisplay(pizzaElement, newSize){
 // Task 3
 // define the function updateOutputColor here
 function updateOutputColor(size1, size2){
+  console.log(size1);
+  console.log(size2);
   if(size1 > size2){
     outputSection.style.backgroundColor = "var(--red)";
   }else{
