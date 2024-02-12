b305d744f98f6c37949d5d38fb69f72651f701a5
Markus <markus.sibbe@t-online.de>
2024-02-12 15:13:48 +0100

    add js-inputs-and-strings/multiple-inputs

--- js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,11 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+button.addEventListener("click", () =>{
+    const firstInputText = firstInput.value;
+    const secondInputText = secondInput.value;
+
+    secondInput.value = firstInputText.toUpperCase();
+    firstInput.value = secondInputText;
+});
