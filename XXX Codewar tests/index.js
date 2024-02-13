e82a2aa147095ab067fc7b237c25bf5a217858a4
Markus <markus.sibbe@t-online.de>
2024-02-13 12:57:30 +0100

    codewars

--- XXX Codewar tests/index.js
+++ XXX Codewar tests/index.js
@@ -206,4 +206,30 @@ function cakes(recipe, available){
 
 
 
+function isSolved(board){
+    for(let i = 0; i < 3; i++){
+        if((board[i][0] === board[i][1] && board[i][1] === board[i][2]) || (board[0][i] === board[1][i] && board[1][i] === board[2][i])){
+            if(board[i][i] === 1 || board[i][i] === 2){
+                return board[i][i];
+            }
+        }
+    }
+    if (
+        (board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
+        (board[0][2] === board[1][1] && board[1][1] === board[2][0])
+    ) {
+        if(board[1][1] === 1 || board[1][1] === 2){
+            return board[1][1];
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == 0) {
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
 
+//console.log(isSolved([  [0,1,1],[2,0,2],[2,1,0]]));
