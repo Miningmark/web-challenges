ccb1970ce7134a09ddd5b91a81b0d737653a3317
Markus <markus.sibbe@t-online.de>
2024-02-13 15:53:43 +0100

    add js-forms/checkbox-input

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -12,13 +12,40 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+tosCheckbox.addEventListener("input", () => {
+  if(tosCheckbox.checked){
+    hideTosError();
+  }else{
+    showTosError()
+  }
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const data = {
+    firstName: event.target.elements.firstName.value,
+    lastName: event.target.elements.lastName.value,
+    age: event.target.elements.age.value,
+    email: event.target.elements.email.value,
+    whatWentWrong: event.target.elements.complaint.value,
+    aboutComplaint: event.target.elements.details.value,
+    experience: event.target.elements.badness.value,
+    date: event.target.elements.orderDate.value,
+    tos: event.target.elements.tos.checked,
+  };
 
+  if(data.tos){
+    hideTosError();
+    alert("Form submitted");
+    event.target.elements.firstName.focus();
+    event.target.reset();
+  }else{
+    showTosError()
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  //alert("Form submitted");
 });
