b4a281541d2fb362d19a56909b0b60840fca78a3
Markus <markus.sibbe@t-online.de>
2024-02-16 11:27:04 +0100

    add js-array-methods-1/filter

--- js-array-methods_filter/index.js
+++ js-array-methods_filter/index.js
@@ -23,13 +23,17 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => card.id ==2);
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => card.tags.length == 3);
+console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => !card.isBookmarked);
+console.log(allCardsThatAreNotBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => (card.isBookmarked && card.tags.includes("js")) || (card.isBookmarked && card.tags.includes("html")));
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
