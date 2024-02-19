4c751639004eb6053ed4453ac51b4c0697b79489
Markus <markus.sibbe@t-online.de>
2024-02-19 15:48:54 +0100

    add js-fetch/star-wars-console

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,15 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    
+    return data.results;
+}
 
-fetchData();
+const starwarsDB = await fetchData();
+console.log(starwarsDB);
+const r2d2EyeColor = starwarsDB.find((character) => character.name == "R2-D2").eye_color;
+console.log("R2-D2 Eyecolor" ,r2d2EyeColor);
