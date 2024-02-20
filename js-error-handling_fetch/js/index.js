e7b9c4ea846cac27ce049b3a4f959004a0266ce0
Markus <markus.sibbe@t-online.de>
2024-02-20 10:55:19 +0100

    add js-error-handling/fetch

--- js-error-handling_fetch/js/index.js
+++ js-error-handling_fetch/js/index.js
@@ -1,19 +1,40 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorElement = document.querySelector(".error");
 
 async function getUser(url) {
-  const response = await fetch(url);
-  const json = await response.json();
-  return json.data;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+      if (response.status === 404) {
+        throw new Error("User not found");
+      } else {
+        throw new Error(`Failed to fetch: ${response.statusText}`);
+      }
+    }
+
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    errorElement.innerText = `Error: ${error.message}`;
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     const user = await getUser(event.target.dataset.url);
-    userElement.innerHTML = `
-    <h2>${user.first_name} ${user.last_name}</h2>
-    <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+    if(user === null){
+      userElement.innerHTML = "User info will appear here.";
+    }else{
+      userElement.innerHTML = `
+      <h2>${user.first_name} ${user.last_name}</h2>
+      <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
     `;
+    errorElement.innerText = ""; 
+    }
   })
 );
