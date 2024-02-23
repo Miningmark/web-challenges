50059dc93dc0f4bb8a06e44b2000db3b897158d8
Markus <markus.sibbe@t-online.de>
2024-02-23 15:47:57 +0100

    add react-props/button

--- react-props_button/src/App.js
+++ react-props_button/src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button textColor="blue" disabled={false} text="Hello" buttonClick={handleClick}/>;
+}
+
+function Button({ textColor, disabled, text, buttonClick }) {
+  const button = (
+    <button
+      style={{
+        backgroundColor: "lightblue",
+        color: textColor,
+        height: "100px",
+      }}
+      disabled={disabled}
+      onClick={buttonClick}
+    >
+      {text}
+    </button>
+  );
+  return button;
+}
+
+function handleClick(event){
+  console.log(event); 
+  alert("You clicked me!");
 }
