f23960c2185dcf109fdc8984d68f89d00975cc90
Markus <markus.sibbe@t-online.de>
2024-02-23 12:23:32 +0100

    add react-basics/hello-world

---
+++ react-basics_hello-world/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorld/>;
+}
+
+
+function HelloWorld(){
+  const element = <h1>Say hello...</h1>;
+
+  return element;
+}
