07b99fcb8019af56869b345c8e7edc49de5f8689
Markus <markus.sibbe@t-online.de>
2024-02-23 12:36:34 +0100

    add react-basics/hello-world-article

--- react-basics_hello-world-article/src/App.js
+++ react-basics_hello-world-article/src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return <HelloWorldArticle/>;
+}
+
+
+function HelloWorldArticle(){
+  const article = (<article><Heading/><Paragraph/></article>);
+  return article;
+}
+
+function Heading() {
+  const heading = <h1>Hello World!</h1>
+  return heading;
+}
+function Paragraph() {
+  const paragraph = <p>I&apos;m a paragraph!</p>;
+  return paragraph;
 }
