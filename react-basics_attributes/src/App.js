a88d182cdc3812a6f95e5ef2098ca2b45e02a969
Markus <markus.sibbe@t-online.de>
2024-02-23 12:59:44 +0100

    add react-basics/attributes

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article/>;
+}
+
+function Article() {
+  const article = <article className="article"><Heading/><Label/><Input/><Anchor/></article>;
+  
+  return article;
+}
+
+function Heading(){
+  const heading = <h2 className="article__title">Hello World</h2>;
+  return heading;
+}
+function Label(){
+  const label = <label htmlFor="inputField">Im a Label </label>;
+  return label;
+}
+function Input(){
+  const input = <input id="inputField"></input>;
+  return input;
+}
+function Anchor(){
+  const anchor = <a className="article__link" href="https://github.com/Miningmark" target="_black"> Im a Anchor</a>;
+  return anchor;
 }
