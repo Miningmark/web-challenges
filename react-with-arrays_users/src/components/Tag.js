acb9b242de2ea0899e9300608a7d3ac1f6591af9
Markus <markus.sibbe@t-online.de>
2024-02-27 11:16:12 +0100

    add react-with-arrays/users

--- react-with-arrays_users/src/components/Tag.js
+++ react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,7 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const isHighlightTag = (tag === 'admin');
+  console.log(tag, isHighlightTag);
+  return <li className={`tag ${isHighlightTag ? 'tag--highlight' : ''}`}>{tag}</li>;
 }
