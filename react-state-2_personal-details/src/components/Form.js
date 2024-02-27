07fe099cd6015c829f06da80b0e2bd667a056217
Markus <markus.sibbe@t-online.de>
2024-02-27 16:15:23 +0100

    add react-state-2/personal-details

--- react-state-2_personal-details/src/components/Form.js
+++ react-state-2_personal-details/src/components/Form.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-export default function Form() {
+export default function Form({onCreateUser}) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
+    // Step 6: Call the prop function with form field values
+    onCreateUser(name, email);
+    // Step 7: Reset the form fields
+    setName("");
+    setEmail("");
+    event.target.name.focus();
   }
 
   return (
@@ -16,9 +22,23 @@ export default function Form() {
     >
       <h2 id="user-details">Please enter your details here!</h2>
       <label htmlFor="name">Name: </label>
-      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="John Doe"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
       <label htmlFor="email">Email: </label>
-      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <input  id="email"
+        name="email"
+        type="email"
+        placeholder="john@doe.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required />
       <button className="form__submit-button" type="submit">
         Submit
       </button>
