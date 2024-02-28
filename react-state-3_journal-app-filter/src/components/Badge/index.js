f13980b281b1a1b9e16cf6beba2ebd2e37fb7c9d
Markus <markus.sibbe@t-online.de>
2024-02-28 14:49:07 +0100

    add react-state-3/journal-app-filter

--- react-state-3_journal-app-filter/src/components/Badge/index.js
+++ react-state-3_journal-app-filter/src/components/Badge/index.js
@@ -1,6 +1,7 @@
 import "./Badge.css";
 
 export default function Badge({ children, isActive }) {
+  console.log(children);
   return (
     <span className={`badge${isActive ? " badge--active" : ""}`}>
       {children}
--- react-state-3_journal-app-filter/src/components/EntriesSection/index.js
+++ react-state-3_journal-app-filter/src/components/EntriesSection/index.js
@@ -5,20 +5,31 @@ import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
+import {useState} from "react";
 
 export default function EntriesSection({ entries, onToggleFavorite }) {
+  const [filter, setFilter] = useState(false);
+
+  function handleSetFilter(){
+    
+  }
+
+  function displayEntries(){
+    return filter ? entries.filter((entry) => entry.isFavorite) : entries;
+  }
+
   return (
     <section className="entries-section">
       <Tabs>
         <Tab active>
-          All Entries <Badge isActive>3</Badge>
+          All Entries <Badge isActive>{entries.length}</Badge>
         </Tab>
         <Tab>
-          Favorites <Badge>1</Badge>
+          Favorites <Badge>{entries.filter((entry) => entry.isFavorite).length}</Badge>
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        {entries.map((entry, index) => (
+        {displayEntries().map((entry, index) => (
           <Fragment key={entry.id}>
             {index > 0 ? <Divider /> : null}
             <Entry
