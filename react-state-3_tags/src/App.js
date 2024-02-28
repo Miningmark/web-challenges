047dd62a664457677a5f52ba8d69a60ffbd04491
Markus <markus.sibbe@t-online.de>
2024-02-28 11:37:15 +0100

    add react-state-3/tags

--- react-state-3_tags/src/App.js
+++ react-state-3_tags/src/App.js
@@ -6,10 +6,18 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTags(newTag){
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTags(deleteTag){
+    setTags(tags.filter((tag) => tag !== deleteTag));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTags}/>
+      <List tags={tags} onDeleteTag={handleDeleteTags} />
     </main>
   );
 }
