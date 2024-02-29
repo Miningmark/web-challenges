fd3e20f8a7bbdf47cc98fa14bf8f4bc7cc833292
Markus <markus.sibbe@t-online.de>
2024-02-29 10:19:08 +0100

    add react-with-local-storage/dice

--- react-with-local-storage_dice/src/components/D6Button/index.js
+++ react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,19 +1,11 @@
-import { useState } from "react";
-import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
-
-  const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
-  };
-
+export function D6Button({ onRolls, rolls }) {
   const currentRollValue = rolls[0]?.value;
 
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
+    <button className="d6-button" type="button" onClick={onRolls}>
       <D6 value={currentRollValue} />
     </button>
   );
