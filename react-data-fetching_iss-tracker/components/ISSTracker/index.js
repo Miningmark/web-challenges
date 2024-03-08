aef94af40360785a3a24819d4b7862bda7b92a6a
Markus <markus.sibbe@t-online.de>
2024-03-08 16:12:13 +0100

    add react-data-fetching/iss-tracker

---
+++ react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import useSWR, { SWRConfig } from "swr";
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch ISS coordinates");
+  }
+  return response.json();
+};
+
+export default function ISSTracker() {
+  const { data, error, mutate } = useSWR(URL, fetcher, {
+    refreshInterval: 5000,
+  });
+
+  if (error) {
+    console.error(error);
+  }
+
+  function handleRefresh() {
+    mutate();
+  }
+
+  return (
+    <main>
+      <Map longitude={data?.longitude || 0} latitude={data?.latitude || 0} />
+      <Controls
+        longitude={data?.longitude || 0}
+        latitude={data?.latitude || 0}
+        // Pass the handleRefresh function to onRefresh prop
+        onRefresh={handleRefresh}
+      />
+    </main>
+  );
+}
