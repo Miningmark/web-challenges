3b7a3b5e442c11a801e92e50aff32d45d353e8b0
Markus <markus.sibbe@t-online.de>
2024-03-14 00:34:18 +0100

    add backend-api-routes_random-character

--- backend-api-routes_random-character/pages/api/random-character.js
+++ backend-api-routes_random-character/pages/api/random-character.js
@@ -2,5 +2,13 @@ import Chance from "chance";
 
 export default function handle(request, response) {
   const chance = new Chance();
-  const character = { name: chance.name(), age: chance.age };
+  const character = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    age: chance.age(),
+    city: chance.city(),
+    email: chance.email(),
+    id: chance.geohash(),
+  };
+  response.status(200).json(character);
 }
--- backend-api-routes_random-character/pages/index.js
+++ backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,22 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error, isLoading } = useSWR("/api/random-character");
+
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <h1>
+        {data.firstName} {data.lastName}
+      </h1>
+      <p>Age: {data.age}</p>
+      <p>City: {data.city}</p>
+    </>
+  );
 }
