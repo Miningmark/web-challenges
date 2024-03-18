4cd56b12e2eaace3674c7e25c7512117b7b12273
Markus <markus.sibbe@t-online.de>
2024-03-18 11:57:11 +0100

    add backend-update-and-delete/products

---
+++ backend-update-and-delete_products/components/Product/index.js
@@ -0,0 +1,78 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+
+export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    console.log(productData);
+    const respone = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (respone.ok) {
+      mutate();
+      setIsEditMode(false);
+    } else {
+      console.error(respone.error);
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.error(response.status);
+    }
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+
+      {isEditMode ? <ProductForm handleOnSubmit={handleEditProduct} heading="Edit Fish" /> : null}
+
+      <button type="button" onClick={() => handleDeleteProduct()}>
+        Delete
+      </button>
+      <button type="button" onClick={() => setIsEditMode(!isEditMode)}>
+        Edit
+      </button>
+      <br></br>
+      <StyledLink href="/">Back to all</StyledLink>
+    </ProductCard>
+  );
+}
